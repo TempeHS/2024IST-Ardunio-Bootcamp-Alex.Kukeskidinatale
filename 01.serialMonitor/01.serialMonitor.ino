e3ff7094b6061d350d8d1cd911c602efe7d09b80
ArtkidAlex <126206459+ArtkidAlex@users.noreply.github.com>
2023-08-04 11:37:28 +1000

    Complete activity 1

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -17,10 +17,17 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Serial Monitor configured to 9600");
+  Serial.println("--------------------------------");
 
 }
 
 // The loop function runs over and over again forever
 void loop() {
-
+  Serial.print("");
+  Serial.println("-");
+  while(!Serial.available());
+  String myString = Serial.readString();
+  Serial.print(myString);
 }
