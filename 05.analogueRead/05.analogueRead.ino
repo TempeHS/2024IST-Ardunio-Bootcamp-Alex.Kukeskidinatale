852fae49f25442e30f2b6a2809d05676f02d4dd4
ArtkidAlex <126206459+ArtkidAlex@users.noreply.github.com>
2023-08-16 09:41:29 +1000

    activity 5 complete

--- 05.analogueRead/05.analogueRead.ino
+++ 05.analogueRead/05.analogueRead.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Alex Kukeski
   Learning Intention: The students will learn how to wire a variable resistor as a pseudo sensor and read analogue data from that PIN.
   Success Criteria:
     1. I can wire a variable resistor in TINKERCAD
@@ -16,10 +16,27 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/05.serialRead/Bootcamp-serialRead.png
 */
 
-void setup() {
+unsigned static int myPOT = A0;
+unsigned static int lightSensor = A1;
+unsigned static int soundSensor = A2;
 
+void setup() {
+Serial.begin(9600);
+//Serial.println("Serial monitor configured to 9600");
+//Serial.println("------------------------------");
 }
 
 void loop() {
-
+ unsigned int POTval = analogRead(myPOT);
+   unsigned int LITval = analogRead(lightSensor);
+    unsigned int SNDval = analogRead(soundSensor);
+Serial.print("myPOT:");
+Serial.print(POTval);
+Serial.print(",");
+Serial.print("lightSensor:");
+Serial.print(LITval);
+Serial.print(",");
+Serial.print("soundSensor:");
+Serial.print(SNDval);
+Serial.println();
 }
